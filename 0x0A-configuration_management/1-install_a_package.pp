26ed0aecaca837db0cbbd0fc11a079403b2a1d5f
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-01 12:31:13 +0100

    1pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
-
-# Ensure Python pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
 # Install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => pip3,
-  require  => Package['python3-pip'],
+  provider => 'pip3',
 }
