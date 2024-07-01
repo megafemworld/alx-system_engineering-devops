a6f020da310f14e08706e263d36a85d33ef50a0f
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-01 12:01:25 +0100

    1 correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,3 @@
-# 1-install_a_package.pp
-
-# Ensure Python pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
 # Install Flask using pip3
 package { 'Flask':
   ensure   => '2.1.0',
