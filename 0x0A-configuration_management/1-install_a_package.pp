c529d86cb2fa6e4a06cac0b56b4579d48775022b
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-01 13:27:25 +0100

    executex1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+# 1-install_a_package.pp
+
+exec { 'install_flask':
+  command => '/usr/bin/apt-get -y install flask= -v 2.1.0',
 }
+
