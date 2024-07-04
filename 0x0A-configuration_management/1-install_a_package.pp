212b77cfbe207ab6b28b458fe3a126bf7449393e
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-04 10:45:25 +0100

    1 solution

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-#install flask
-package { 'flask':
+#install flask version 2.1.0
+
+
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
