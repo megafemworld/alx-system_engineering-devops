746529aa03bdc09124139d1e52164b28bf00b41a
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-04 10:41:15 +0100

    11

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# 1-install_a_package.pp
-
-exec { 'install_flask':
-  command => '/usr/bin/apt-get -y install flask= -v 2.1.0',
-}
+#Install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
