e8c635a43c6cbdc762b0cdd54a5dec23a0286a6d
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-19 11:15:03 +0100

    puppet running it

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+#install nginix with puppet
+#listening on port 80
+#when queried as root it must return Hello world
+#redirection is moved permenately
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
