1dddbb709c0d6a89c32679a522bea33bf15428f6
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-02 08:57:32 +0100

    to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" API returns information about his/her TODO list progress."""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    import json
+
+    id = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    todo_url = f"{base_url}/todos?userId={id}"
+    user_url = f"{base_url}/users/{id}"
+    un = requests.get(user_url).json().get('username')
+    filename = f"{id}.json"
+    todos = requests.get(todo_url).json()
+    tasks = []
+    for todo in todos:
+        task = {
+            "task": todo["title"],
+            "completed": todo["completed"],
+            "username": un
+        }
+        tasks.append(task)
+    data = {str(id) : tasks}
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
