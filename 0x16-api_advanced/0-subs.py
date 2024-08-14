9bdb0b23968a53654ee15f5be5a2c0d26979298d
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-14 02:37:05 +0100

    0 sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-if an invalid subreddit is given, the function should return 0.
-"""
+""" 0-subs.py """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API"""
-    headers = {
-        "User-Agent": "0x16. API_advanced-megafemgroup"
-    }
+    """ number_of_subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    else:
         return 0
-    sub = response.json().get("data").get("subscribers")
-    return sub
