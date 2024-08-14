f7b417ce1786228d3c965bd06189de9183b030ec
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-14 02:39:02 +0100

    0 sub 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-""" 0-subs.py """
+"""function that returns the number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ number_of_subscribers """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    """Gets number of subscribers
+       Args:
+           subreddit (str): name of subreddit
+       Returns:
+           number of subscribers if valid, 0 otherwise
+    """
+    base_url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/about'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    else:
+    if response.status_code != 200:
         return 0
+
+    about_dict = response.json()
+
+    return about_dict['data']['subscribers']
